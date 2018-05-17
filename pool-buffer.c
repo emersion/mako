cb9163077e66ca0a8ee54c6585c9e3733cffa2da
emersion <contact@emersion.fr>
2018-05-17 11:32:19 +0100

    Set FD_CLOEXEC on buffer FDs

--- pool-buffer.c
+++ pool-buffer.c
@@ -1,5 +1,6 @@
 #define _XOPEN_SOURCE 500
 #include <cairo/cairo.h>
+#include <fcntl.h>
 #include <pango/pangocairo.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,26 +8,45 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <wayland-client.h>
+
 #include "pool-buffer.h"
+
+static bool set_cloexec(int fd) {
+	long flags = fcntl(fd, F_GETFD);
+	if (flags == -1) {
+		return false;
+	}
+
+	if (fcntl(fd, F_SETFD, flags | FD_CLOEXEC) == -1) {
+		return false;
+	}
+
+	return true;
+}
 
 static int create_pool_file(size_t size, char **name) {
 	static const char template[] = "mako-XXXXXX";
 	const char *path = getenv("XDG_RUNTIME_DIR");
-	if (!path) {
+	if (path == NULL) {
+		fprintf(stderr, "XDG_RUNTIME_DIR is not set\n");
 		return -1;
 	}
 
-	int ts = (path[strlen(path) - 1] == '/');
-
-	*name = malloc(
-		strlen(template) +
-		strlen(path) +
-		(ts ? 0 : 1) + 1);
-	sprintf(*name, "%s%s%s", path, ts ? "" : "/", template);
+	size_t name_size = strlen(template) + 1 + strlen(path) + 1;
+	*name = malloc(name_size);
+	if (*name == NULL) {
+		fprintf(stderr, "allocation failed\n");
+		return -1;
+	}
+	snprintf(*name, name_size, "%s/%s", path, template);
 
 	int fd = mkstemp(*name);
-
 	if (fd < 0) {
+		return -1;
+	}
+
+	if (!set_cloexec(fd)) {
+		close(fd);
 		return -1;
 	}
 
@@ -38,13 +58,13 @@ static int create_pool_file(size_t size, char **name) {
 	return fd;
 }
 
-static void buffer_release(void *data, struct wl_buffer *wl_buffer) {
+static void buffer_handle_release(void *data, struct wl_buffer *wl_buffer) {
 	struct pool_buffer *buffer = data;
 	buffer->busy = false;
 }
 
 static const struct wl_buffer_listener buffer_listener = {
-	.release = buffer_release
+	.release = buffer_handle_release,
 };
 
 static struct pool_buffer *create_buffer(struct wl_shm *shm,
