67f2ed1862a94b1923acd0fc83df18ad03c58a28
Simon Ser <contact@emersion.fr>
2020-11-09 22:22:20 +0100

    build: introduce sd-bus-provider option

--- include/event-loop.h
+++ include/event-loop.h
@@ -5,9 +5,9 @@
 #include <stdbool.h>
 #include <time.h>
 #include <wayland-client.h>
-#ifdef HAVE_SYSTEMD
+#ifdef HAVE_LIBSYSTEMD
 #include <systemd/sd-bus.h>
-#elif HAVE_ELOGIND
+#elif HAVE_LIBELOGIND
 #include <elogind/sd-bus.h>
 #endif
 
