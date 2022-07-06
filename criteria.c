emersion/mako

commit df298c8fea910e4deb8132aca4e0d6079e8888d3

Use an array to store current modes

criteria.c
@@ -11,6 +11,7 @@
 #include "mako.h"
 #include "config.h"
 #include "criteria.h"
+#include "mode.h"
 #include "notification.h"
 #include "surface.h"
 #include "wayland.h"
@@ -152,7 +153,7 @@ bool match_criteria(struct mako_criteria *criteria,
 		return false;
 	}
 
-	if (spec.mode && strcmp(criteria->mode, notif->state->current_mode) != 0) {
+	if (spec.mode && has_mode(notif->state, criteria->mode)) {
 		return false;
 	}
 

dbus/mako.c
@@ -9,6 +9,7 @@
 #include "surface.h"
 #include "dbus.h"
 #include "mako.h"
+#include "mode.h"
 #include "notification.h"
 #include "wayland.h"
 
@@ -312,8 +313,7 @@ static int handle_set_mode(sd_bus_message *msg, void *data,
 		return ret;
 	}
 
-	free(state->current_mode);
-	state->current_mode = strdup(mode);
+	set_modes(state, &mode, 1);
 
 	reapply_config(state);
 

include/mako.h
@@ -74,7 +74,7 @@ struct mako_state {
 	uint32_t last_id;
 	struct wl_list notifications; // mako_notification::link
 	struct wl_list history; // mako_notification::link
-	char *current_mode;
+	struct wl_array current_modes; // char *
 
 	int argc;
 	char **argv;

include/mode.h
@@ -1,0 +1,11 @@
+#ifndef MODE_H
+#define MODE_H
+
+#include <stdbool.h>
+
+struct mako_state;
+
+bool has_mode(struct mako_state *state, const char *mode);
+void set_modes(struct mako_state *state, const char **modes, size_t modes_len);
+
+#endif

main.c
@@ -7,6 +7,7 @@
 #include "config.h"
 #include "dbus.h"
 #include "mako.h"
+#include "mode.h"
 #include "notification.h"
 #include "render.h"
 #include "surface.h"
@@ -72,12 +73,18 @@ static bool init(struct mako_state *state) {
 	}
 	wl_list_init(&state->notifications);
 	wl_list_init(&state->history);
-	state->current_mode = strdup("default");
+	wl_array_init(&state->current_modes);
+	const char *mode = "default";
+	set_modes(state, &mode, 1);
 	return true;
 }
 
 static void finish(struct mako_state *state) {
-	free(state->current_mode);
+	char **mode_ptr;
+	wl_array_for_each(mode_ptr, &state->current_modes) {
+		free(*mode_ptr);
+	}
+	wl_array_release(&state->current_modes);
 
 	struct mako_notification *notif, *tmp;
 	wl_list_for_each_safe(notif, tmp, &state->notifications, link) {

meson.build
@@ -61,6 +61,7 @@ src_files = [
 	'dbus/mako.c',
 	'dbus/xdg.c',
 	'main.c',
+	'mode.c',
 	'notification.c',
 	'pool-buffer.c',
 	'render.c',

mode.c
@@ -1,0 +1,30 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdlib.h>
+#include <string.h>
+#include <wayland-util.h>
+
+#include "mako.h"
+#include "mode.h"
+
+bool has_mode(struct mako_state *state, const char *mode) {
+	const char **mode_ptr;
+	wl_array_for_each(mode_ptr, &state->current_modes) {
+		if (strcmp(*mode_ptr, mode) == 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void set_modes(struct mako_state *state, const char **modes, size_t modes_len) {
+	char **mode_ptr;
+	wl_array_for_each(mode_ptr, &state->current_modes) {
+		free(*mode_ptr);
+	}
+	state->current_modes.size = 0;
+
+	for (size_t i = 0; i < modes_len; i++) {
+		char **dst = wl_array_add(&state->current_modes, sizeof(char *));
+		*dst = strdup(modes[i]);
+	}
+}

commit 413c160e1b0d5bc72cc57de68f5cb4482b6c5809

Fix inverted has_mode condition
Fixes: df298c8fea91 ("Use an array to store current modes")
Closes: https://github.com/emersion/mako/issues/437

criteria.c
@@ -153,7 +153,7 @@ bool match_criteria(struct mako_criteria *criteria,
 		return false;
 	}
 
-	if (spec.mode && has_mode(notif->state, criteria->mode)) {
+	if (spec.mode && !has_mode(notif->state, criteria->mode)) {
 		return false;
 	}
 